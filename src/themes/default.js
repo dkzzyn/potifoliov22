d0e1b8d5409508d6b47b124035776a81ef69cd47
Diogo Silva Ferreira <diogosiilvaff@gmail.com>
2025-03-20 12:14:01 -0300

    first commit

---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+  // Temp fonts
+  fonts: {
+    title: "Space Grotesk, sans-serif",
+    main: "Space Grotesk, sans-serif"
+  },
+  // Colors for layout
+  colors: {
+    primary1: "#00509D", // Azul vibrante
+    background1: "#003366", // Azul escuro
+    button: "#0066CC", // Azul vibrante
+    background2: "#001F5D", // Azul profundo
+    text: "#B0C4DE", // Azul claro acinzentado
+    text1: "#ADD8E6", // Azul quase branco
+    text2: "#6495ED", // Azul médio suave
+    text3: "#4682B4", // Azul acinzentado
+    footerBackground: "#001F3F" // Azul bem escuro
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+}
